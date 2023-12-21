386065f4699c5e533417390607d1be85c108f055
Sohrab Alam <sohrab.alam86@gmail.com>
2023-12-21 12:31:26 +0530

    1. Password generated in console. Default User name is user.

---
+++ spring-security-level-1/src/main/java/com/easybytes/springsecuritylevel1/SpringSecurityLevel1Application.java
@@ -0,0 +1,13 @@
+package com.easybytes.springsecuritylevel1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLevel1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLevel1Application.class, args);
+	}
+
+}
---
+++ spring-security-level-1/src/test/java/com/easybytes/springsecuritylevel1/SpringSecurityLevel1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easybytes.springsecuritylevel1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLevel1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
