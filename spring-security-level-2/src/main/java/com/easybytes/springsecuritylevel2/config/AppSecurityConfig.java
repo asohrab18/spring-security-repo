6307084d2a42a5cd01cf55057d09994ce78cd7b7
Sohrab Alam <sohrab.alam86@gmail.com>
2023-12-21 14:09:13 +0530

    Approach2: used deprecated NoOpPasswordEncoder

--- spring-security-level-2/src/main/java/com/easybytes/springsecuritylevel2/config/AppSecurityConfig.java
+++ spring-security-level-2/src/main/java/com/easybytes/springsecuritylevel2/config/AppSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -22,16 +24,25 @@ public class AppSecurityConfig {
 	}
 	
 	
-	@SuppressWarnings("deprecation")
 	@Bean
 	public InMemoryUserDetailsManager userDetailsService() {
-		//Approach 1: Here, we save users credentials in application's memory.
+		//Deprecated Approach 1: Here, we save users credentials in application's memory with DefaultPasswordEncoder.
+		/*
 		UserDetails user1 = User.withDefaultPasswordEncoder().username("user1").password("pass1").authorities("admin").build();
-
 		UserDetails user2 = User.withDefaultPasswordEncoder().username("user2").password("pass2").authorities("read-write").build();
-
 		UserDetails user3 = User.withDefaultPasswordEncoder().username("user3").password("pass3").authorities("read-only").build();
-
+		*/
+		
+		//Approach 2: Here, we save users credentials in application's memory with deprecated NoOpPasswordEncoder.
+		UserDetails user1 = User.withUsername("user1").password("pass1").authorities("admin").build();
+		UserDetails user2 = User.withUsername("user2").password("pass2").authorities("read-write").build();
+		UserDetails user3 = User.withUsername("user3").password("pass3").authorities("read-only").build();
+		
 		return new InMemoryUserDetailsManager(user1, user2, user3);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 }
