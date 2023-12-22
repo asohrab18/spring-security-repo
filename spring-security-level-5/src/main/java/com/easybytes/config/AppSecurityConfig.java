9b56569b7a7f618c70e9d61be09277f850190aa7
Sohrab Alam <sohrab.alam86@gmail.com>
2023-12-22 14:48:00 +0530

    Used BCryptPasswordEncoder to save hashed password in DB.

--- spring-security-level-5/src/main/java/com/easybytes/config/AppSecurityConfig.java
+++ spring-security-level-5/src/main/java/com/easybytes/config/AppSecurityConfig.java
@@ -4,13 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AppSecurityConfig {
 
+	@SuppressWarnings("removal")
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeHttpRequests(
@@ -23,6 +24,6 @@ public class AppSecurityConfig {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
 }
--- spring-security-level-5/src/main/java/com/easybytes/controller/RegistrationController.java
+++ spring-security-level-5/src/main/java/com/easybytes/controller/RegistrationController.java
@@ -3,6 +3,7 @@ package com.easybytes.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,20 +17,22 @@ public class RegistrationController {
 	@Autowired
 	private CustomerRepository customerRepository;
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@PostMapping("register")
 	public ResponseEntity<String> sayWelcome(@RequestBody Customer customer) {
 		ResponseEntity<String> responseEntity = null;
 		Customer savedCustomer = null;
 		try {
+			String hashedPwd = passwordEncoder.encode(customer.getPwd());
+			customer.setPwd(hashedPwd);
 			savedCustomer = customerRepository.save(customer);
 			if (savedCustomer.getId() > 0) {
-				responseEntity = ResponseEntity
-						.status(HttpStatus.CREATED)
-						.body("Registered successfully.");
+				responseEntity = ResponseEntity.status(HttpStatus.CREATED).body("Registered successfully.");
 			}
 		} catch (Exception e) {
-			responseEntity = ResponseEntity
-					.status(HttpStatus.INTERNAL_SERVER_ERROR)
+			responseEntity = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
 					.body("an exception occurred due to " + e.getMessage());
 
 		}
