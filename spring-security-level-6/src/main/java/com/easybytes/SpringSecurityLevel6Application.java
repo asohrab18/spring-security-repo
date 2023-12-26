6807f6ade5049b08bacc50af652bf7cedd01b106
Sohrab Alam <sohrab.alam86@gmail.com>
2023-12-26 20:33:11 +0530

    Updated

---
+++ spring-security-level-6/src/main/java/com/easybytes/SpringSecurityLevel6Application.java
@@ -0,0 +1,12 @@
+package com.easybytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLevel6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLevel6Application.class, args);
+	}
+}
