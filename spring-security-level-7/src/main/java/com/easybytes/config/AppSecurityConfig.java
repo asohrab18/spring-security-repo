7a9779f9a811dd3d0f2ca5d60ddbb54679eb651d
Sohrab Alam <sohrab.alam86@gmail.com>
2023-12-29 13:06:40 +0530

    CORS & CSRF Handling

--- spring-security-level-7/src/main/java/com/easybytes/config/AppSecurityConfig.java
+++ spring-security-level-7/src/main/java/com/easybytes/config/AppSecurityConfig.java
@@ -6,11 +6,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+
+import com.easybytes.filter.CsrfCookieFilter;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -19,7 +25,12 @@ public class AppSecurityConfig {
 
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http.cors().configurationSource(new CorsConfigurationSource() {
+		CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+		csrfTokenRequestAttributeHandler.setCsrfRequestAttributeName("_csrf");
+
+        http.securityContext().requireExplicitSave(false).and()
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+		.cors().configurationSource(new CorsConfigurationSource() {
 			@Override
 			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 				CorsConfiguration config = new CorsConfiguration();
@@ -30,8 +41,12 @@ public class AppSecurityConfig {
 				config.setMaxAge(3600L);
 				return config;
 			}
-		}).and().csrf().ignoringRequestMatchers("/contact", "/register")
-		.and().authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards")
+		}).and().csrf(csrf -> csrf.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+				.ignoringRequestMatchers("/contact", "/register")
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+		.addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+
+		.authorizeHttpRequests(requests -> requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards")
 								.authenticated().requestMatchers("/notices", "/contact", "/register").permitAll())
 				.formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
 		return http.build();
